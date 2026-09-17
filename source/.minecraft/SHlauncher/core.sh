bcda25c298ae55ca41fd4fb587be8811072978c0
LeRouge53 <adritz.tritz.1@proton.me>
2026-09-17 13:32:14 +0200

    Finished renovating launch.sh (it is now way cleaner)

--- source/.minecraft/SHlauncher/core.sh
+++ source/.minecraft/SHlauncher/core.sh
@@ -90,7 +90,7 @@ function commandLineHandler() {
 	;;
 	"launch")
 		# shellcheck source=commands/launch.sh
-		source "$cmdDir/launch2.sh" "$@"
+		source "$cmdDir/launch.sh" "$@"
 		cmdExitCode=$?
 	;;
 	"opendir")
